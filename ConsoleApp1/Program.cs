d81dbb2718e4225683e4ee06aa61b798c7ddb2f8
Carlos Rosales <crosales@kz.cl>
2020-04-08 19:07:47 -0400

    commit inicial por crosales
    crosales

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "esto es una prueba"; // <---- el mensaje a enviar
+            string password = "t3stCarl0sR0sales"; // <---- la clave que tendremos ambos
+
+            SHA256 mySHA256 = SHA256Managed.Create();
+            byte[] key = mySHA256.ComputeHash(Encoding.ASCII.GetBytes(password));
+
+            byte[] iv = new byte[16] { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+
+            string encrypted = EncryptString(message, key, iv);
+            string decrypted = DecryptString(encrypted, key, iv);
+
+            Console.WriteLine(encrypted);  // <---- esto me enviaras como numero de tarjeta
+            Console.WriteLine(decrypted);
+        }
+
+        public static string EncryptString(string plainText, byte[] key, byte[] iv)
+        {
+            Aes encryptor = Aes.Create();
+
+            encryptor.Mode = CipherMode.CBC;
+
+            byte[] aesKey = new byte[32];
+            Array.Copy(key, 0, aesKey, 0, 32);
+            encryptor.Key = aesKey;
+            encryptor.IV = iv;
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            ICryptoTransform aesEncryptor = encryptor.CreateEncryptor();
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, aesEncryptor, CryptoStreamMode.Write);
+
+            byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
+
+            cryptoStream.Write(plainBytes, 0, plainBytes.Length);
+
+            cryptoStream.FlushFinalBlock();
+
+            byte[] cipherBytes = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
+
+            return cipherText;
+        }
+
+        public static string DecryptString(string cipherText, byte[] key, byte[] iv)
+        {
+            Aes encryptor = Aes.Create();
+
+            encryptor.Mode = CipherMode.CBC;
+
+            byte[] aesKey = new byte[32];
+            Array.Copy(key, 0, aesKey, 0, 32);
+            encryptor.Key = aesKey;
+            encryptor.IV = iv;
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            ICryptoTransform aesDecryptor = encryptor.CreateDecryptor();
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, aesDecryptor, CryptoStreamMode.Write);
+
+            string plainText = String.Empty;
+
+            try
+            {
+                byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+                cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
+
+                cryptoStream.FlushFinalBlock();
+
+                byte[] plainBytes = memoryStream.ToArray();
+
+                plainText = Encoding.ASCII.GetString(plainBytes, 0, plainBytes.Length);
+            }
+            finally
+            {
+                memoryStream.Close();
+                cryptoStream.Close();
+            }
+            return plainText;
+        }
+    }
+}
